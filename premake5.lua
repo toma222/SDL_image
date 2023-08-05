1eed64c589b5d1ca4f648ee4d8805eeb87144671
toma222 <aidannwilhite@gmail.com>
2023-08-05 16:35:17 +0000

    added premake5

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "SDL_image"
+	kind "SharedLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "NDEBUG"
+		runtime "Release"
+		optimize "on"
